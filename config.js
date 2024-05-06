5aff85bcbe98f85433be8b7f50f21740600aa368
Manikanta Muppidi <128898564+Mani-M-1@users.noreply.github.com>
2024-05-06 09:21:32 +0530

    removde procfile and it's configuration

--- config.js
+++ config.js
@@ -1,50 +1,9 @@
-// const dotenv = require('dotenv');
-// const path = require('path');
-
-// dotenv.config({
-//     path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
-// });
-
-// module.exports = {
-//     NODE_ENV : process.env.NODE_ENV || 'development',
-//     HOST: process.env.HOST || 'localhost',
-//     PORT : process.env.PORT || 3000,
-//     MONGOOSE_URI: process.env.MONGOOSE_URI
-// }
-
-
 const dotenv = require('dotenv');
 const path = require('path');
-const fs = require('fs');
 
 dotenv.config({
     path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
 });
-
-
-// Determine the environment (e.g., based on an environment variable)
-const environment = process.env.NODE_ENV || 'development';
-
-// Determine the command based on the environment
-let command;
-if (environment === 'production') {
-  command = 'npm run prod';
-} else {
-  command = 'npm run dev';
-}
-
-// Generate the Procfile content
-const procfileContent = `web: ${command}`;
-
-// Write the Procfile content to a file
-fs.writeFile('Procfile', procfileContent, (err) => {
-  if (err) {
-    console.error('Error writing to Procfile:', err);
-  } else {
-    console.log('Procfile updated successfully.');
-  }
-});
-
 
 module.exports = {
     NODE_ENV : process.env.NODE_ENV || 'development',
@@ -52,3 +11,44 @@ module.exports = {
     PORT : process.env.PORT || 3000,
     MONGOOSE_URI: process.env.MONGOOSE_URI
 }
+
+
+// const dotenv = require('dotenv');
+// const path = require('path');
+// const fs = require('fs');
+
+// dotenv.config({
+//     path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`)
+// });
+
+
+// // Determine the environment (e.g., based on an environment variable)
+// const environment = process.env.NODE_ENV || 'development';
+
+// // Determine the command based on the environment
+// let command;
+// if (environment === 'production') {
+//   command = 'npm run prod';
+// } else {
+//   command = 'npm run dev';
+// }
+
+// // Generate the Procfile content
+// const procfileContent = `web: ${command}`;
+
+// // Write the Procfile content to a file
+// fs.writeFile('Procfile', procfileContent, (err) => {
+//   if (err) {
+//     console.error('Error writing to Procfile:', err);
+//   } else {
+//     console.log('Procfile updated successfully.');
+//   }
+// });
+
+
+// module.exports = {
+//     NODE_ENV : process.env.NODE_ENV || 'development',
+//     HOST: process.env.HOST || 'localhost',
+//     PORT : process.env.PORT || 3000,
+//     MONGOOSE_URI: process.env.MONGOOSE_URI
+// }
